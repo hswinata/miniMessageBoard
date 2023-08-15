d42da4a67b8e4f32f532a781ddce87c76670b46f
hswinata <harrysurywinata@gmail.com>
2023-08-15 16:27:37 +0200

    Add DELETE route and function

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -19,6 +19,26 @@ exports.list = (req, res, next) => {
   });
 };
 
+//GET a single message:
+exports.message_get = (req, res, next) => {
+  const userId = req.params.id;
+  pool.query(
+    "SELECT * FROM users WHERE id = $1",
+    [userId],
+    (error, results) => {
+      if (error) {
+        throw error;
+      } else {
+        let message = results.rows[0];
+        res.render("message_single", {
+          title: "This is a single message",
+          message: message,
+        });
+      }
+    }
+  );
+};
+
 // Display create message form:
 exports.form = (req, res, next) => {
   res.render("messages_form", {
@@ -29,7 +49,8 @@ exports.form = (req, res, next) => {
 // PUT a new message:
 exports.create_message = (req, res) => {
   const { username, text } = req.body;
-  let date = new Date();
+  let newDate = new Date();
+  let date = newDate.toLocaleString();
 
   pool.query(
     "INSERT INTO users (username, text, date) VALUES ($1, $2, $3) RETURNING * ",
@@ -45,12 +66,15 @@ exports.create_message = (req, res) => {
 
 /*DELETE a message*/
 exports.delete_message = (req, res) => {
-  const id = parseInt(req.params.id);
-
-  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
-    if (error) {
-      throw error;
+  const messageId = req.params.id;
+  pool.query(
+    "DELETE FROM users WHERE id = $1",
+    [messageId],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).redirect("/messages");
     }
-    res.status(200).send(`User deleted with ID: ${id}`);
-  });
+  );
 };
--- routes/messages.js
+++ routes/messages.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const message_controller = require("../controllers/messageController");
 
-// GET catalog of messages:
+// GET messages:
 router.get("/", message_controller.list);
+
+// GET a single message:
+router.get("/:id", message_controller.message_get);
 
 //GET /new page:
 router.get("/new", message_controller.form);
@@ -11,7 +14,7 @@ router.get("/new", message_controller.form);
 //POST /new message:
 router.post("/new", message_controller.create_message);
 
-//DELETE a message:
-router.delete("", message_controller.delete_message);
+//POST request to delete a message:
+router.post("/:id/delete", message_controller.delete_message);
 
 module.exports = router;
